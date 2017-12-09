6572b52ec2f8c073344abffc1325c978c0e30383
best311 <best-311@hotmail.com>
2017-12-09 17:45:10 +0700

    Register

---
+++ IP/Register.php
@@ -0,0 +1,22 @@
+<html>
+<body background="BG.jpg">
+  <font size =8 color = "white">
+    <p align=center> Register </p>
+  </font>
+  <form>
+    <font size = 3 color = "white">
+    <p align = center> Username &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <input type="text" name="username"> </p>
+    <p align = center> Password &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <input type="text" name="password"> </p>
+    <p align = center> Confrim Password &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <input type="text" name="confrimpassword"> </p>
+    <p align = center> E-mail &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <input type="text" name="email"> </p>
+    <center>
+      <button type="button" onclick = "alert('SUCCESS')"> Submit</button>
+    </center>
+  </font>
+  </form>
+</body>
+</html>
