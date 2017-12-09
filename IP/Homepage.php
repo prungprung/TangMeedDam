cba76866f88ba19e2997c19d1fffe0c6c3bcafec
prungprung <prungprung@hotmail.com>
2017-12-09 16:37:55 +0700

    D jaaa

---
+++ IP/Homepage.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Home</title>
+  <?php session_start(); ?>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+  <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+  <link rel="stylesheet" href="Homepage.css">
+
+</head>
+<body background="steel.jpg">
+  <div class="jumbotron jumbotron-fluid" id="headerJumbo">
+    <div class="container" id="jumboHead">
+
+      <div id="picinjumbo">
+<img src="store.jpg" alt="knife" width="2000" height="200">
+      </div>
+    </div>
+  </div>
+
+  <nav class="navbar navbar-expand-md bg-dark navbar-dark" id = "navbar">
+  <ul class="navbar-nav">
+    <li class="nav-item active">
+      <a class="nav-link" href="Home[NoLog].php">หน้าหลัก</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="Login[NoLog].php">สินค้่า</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="Register[NoLog].php">Register</a>
+    </li>
+  </ul>
+</nav>
+<class " cpl-ml-"
+  <div class="leftside" id = "leftside" board = "2">
+    <table style="width:100%" border="2">
+      <tr>
+        <th>Firstname</th>
+      </tr>
+      <tr>
+        <td>Jill</td>
+      </tr>
+      <tr>
+        <td>Eve</td>
+      </tr>
+    </table>
+  </div>
+
+
+</body>
+</html>
